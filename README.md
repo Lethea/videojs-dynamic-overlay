e1c440ef7416ab4f448a258d2ed6c8dc20d73ee9
Emre Karatasoglu <emrek@boryazilim.com>
2017-10-12 10:44:13 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# videojs-newoverlay
+
+Video Üzerine Text Vb Dinamik Yer Değiştirme
+
+## Table of Contents
+
+<!-- START doctoc -->
+<!-- END doctoc -->
+## Installation
+
+```sh
+npm install --save videojs-newoverlay
+```
+
+## Usage
+
+To include videojs-newoverlay on your website or web application, use any of the following methods.
+
+### `<script>` Tag
+
+This is the simplest case. Get the script in whatever way you prefer and include the plugin _after_ you include [video.js][videojs], so that the `videojs` global is available.
+
+```html
+<script src="//path/to/video.min.js"></script>
+<script src="//path/to/videojs-newoverlay.min.js"></script>
+<script>
+  var player = videojs('my-video');
+
+  player.newoverlay();
+</script>
+```
+
+### Browserify/CommonJS
+
+When using with Browserify, install videojs-newoverlay via npm and `require` the plugin as you would any other module.
+
+```js
+var videojs = require('video.js');
+
+// The actual plugin function is exported by this module, but it is also
+// attached to the `Player.prototype`; so, there is no need to assign it
+// to a variable.
+require('videojs-newoverlay');
+
+var player = videojs('my-video');
+
+player.newoverlay();
+```
+
+### RequireJS/AMD
+
+When using with RequireJS (or another AMD library), get the script in whatever way you prefer and `require` the plugin as you normally would:
+
+```js
+require(['video.js', 'videojs-newoverlay'], function(videojs) {
+  var player = videojs('my-video');
+
+  player.newoverlay();
+});
+```
+
+## License
+
+Apache-2.0. Copyright (c) Emre Karatasoglu &lt;emre.karatasoglu@hotmail.com&gt;
+
+
+[videojs]: http://videojs.com/
---
+++ src/plugin.js
@@ -0,0 +1,81 @@
+import videojs from 'video.js';
+import {version as VERSION} from '../package.json';
+
+// Default options for the plugin.
+const defaults = {
+  pozisyon:'sol-ust',
+  icerik:'burda',
+  degisimSuresi:3000
+};
+
+// Cross-compatibility for Video.js 5 and 6.
+const registerPlugin = videojs.registerPlugin || videojs.plugin;
+ const dom = videojs.dom || videojs;
+
+/**
+ * Function to invoke when the player is ready.
+ *
+ * This is a great place for your plugin to initialize itself. When this
+ * function is called, the player will have its DOM and child components
+ * in place.
+ *
+ * @function onPlayerReady
+ * @param    {Player} player
+ *           A Video.js player object.
+ *
+ * @param    {Object} [options={}]
+ *           A plain object containing options for the plugin.
+ */
+const randomArea=(element)=>{
+  var heightOfVideo = window.document.getElementById("videojs-newoverlay-player_html5_api").clientHeight;
+  var widthOfVideo = window.document.getElementById("videojs-newoverlay-player_html5_api").clientWidth
+  var number = Math.floor(Math.random() * widthOfVideo);
+  element.style.left=number+"px";
+  var number2 = Math.floor(Math.random() * heightOfVideo);
+  element.style.top=number2+"px";
+  setTimeout(function(){randomArea(element)},5000);
+};
+const onPlayerReady = (player, options) => {
+  player.addClass('vjs-newoverlay');
+  if(options.pozisyon=="sol-ust"){
+    const el = dom.createEl('div', {
+      className: `vjs-emre`
+    });
+    el.innerHTML=options.icerik;
+    player.el().appendChild(el);
+    player.on("play",function(){
+      el.append("emre");
+      randomArea(el);
+
+    });
+    player.on("timeupdate",function(){
+      //randomArea(el);
+    });
+  }
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function newoverlay
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const newoverlay = function(options) {
+  this.ready(() => {
+    onPlayerReady(this, videojs.mergeOptions(defaults, options));
+  });
+};
+
+// Register the plugin with video.js.
+registerPlugin('newoverlay', newoverlay);
+
+// Include the version number.
+newoverlay.VERSION = VERSION;
+
+export default newoverlay;
